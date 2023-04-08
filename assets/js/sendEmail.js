8a83a4fc78537412831c67ccb7202cdc99a81cd5
Nikkeljohn <nikkeljohn120@gmail.com>
2023-04-08 17:43:20 +0000

    added emailfunction

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("service_7sog2ka","template_8mk8ych", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
